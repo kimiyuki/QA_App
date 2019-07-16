56982a49be2ff401bb92d946ffab29f9bd548efa
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-16 12:19:14 +0900

    wip

--- app/src/main/java/shirai/kimiyuki/techacademy/qa_app/LoginActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/qa_app/LoginActivity.kt
@@ -3,12 +3,13 @@ package shirai.kimiyuki.techacademy.qa_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.renderscript.Sampler
+import android.support.design.widget.Snackbar
 import android.view.View
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -31,11 +32,50 @@ class LoginActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
 
         mCreateAccountListener = OnCompleteListener { task ->
-            //
+            if (task.isSuccessful) {
+                // 成功した場合 ログインを行う
+                val email = emailText.text.toString()
+                val password = passwordText.text.toString()
+                login(email, password)
+            } else {
+                // 失敗した場合  エラーを表示する
+                val view = findViewById<View>(android.R.id.content)
+                Snackbar.make(view, "アカウント作成に失敗しました", Snackbar.LENGTH_LONG).show()
+
+                // プログレスバーを非表示にする
+                progressBar.visibility = View.GONE
+            }
         }
 
         mLoginListener = OnCompleteListener { task ->
-            //
+           if(task.isSuccessful){
+               val user =  mAuth.currentUser
+               val userRef = mDataBaseReference.child(UsersPATH).child(user!!.uid)
+
+               if(mIsCreateAccount){
+                   val data = HashMap<String, String>()
+                   data["name"] = nameText.text.toString()
+                   userRef.setValue(data)
+                   saveName(nameText.text.toString())
+               }else{
+                   userRef.addListenerForSingleValueEvent(object: ValueEventListener{
+                       override fun onCancelled(p0: DatabaseError) {
+                           TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                       }
+
+                       override fun onDataChange(snapshot: DataSnapshot) {
+                           val data = snapshot.value as Map<*, *>?
+                           saveName(data!!["name"]  as String)
+                       }
+                   })
+                   progressBar.visibility = View.GONE
+                   finish()
+               }
+           }else{
+               val view = findViewById<View>(android.R.id.content)
+               Snackbar.make(view, "ログインに失敗しました", Snackbar.LENGTH_LONG)
+               progressBar.visibility = View.GONE
+           }
         }
 
         title = ""
@@ -55,11 +95,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun createAccount(email:String, password:String){
+        // プログレスバーを表示する
+        progressBar.visibility = View.VISIBLE
+
+        // アカウントを作成する
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(mCreateAccountListener)
 
     }
 
     private fun login(email:String, password: String){
+        // プログレスバーを表示する
+        progressBar.visibility = View.VISIBLE
 
+        // ログインする
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(mLoginListener)
     }
 
     private fun saveName(name:String){
