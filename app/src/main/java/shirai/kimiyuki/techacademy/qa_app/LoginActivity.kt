f3aeca29174d54f2af2fe7d1829532b954980021
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-16 12:01:53 +0900

    wip buidling initial set

--- app/src/main/java/shirai/kimiyuki/techacademy/qa_app/LoginActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/qa_app/LoginActivity.kt
@@ -2,11 +2,71 @@ package shirai.kimiyuki.techacademy.qa_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mCreateAccountListener: OnCompleteListener<AuthResult>
+    private lateinit var mLoginListener: OnCompleteListener<AuthResult>
+    private lateinit var mDataBaseReference: DatabaseReference
+
+    // アカウント作成時にフラグを立て、ログイン処理後に名前をFirebaseに保存する
+    private var mIsCreateAccount = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mDataBaseReference = FirebaseDatabase.getInstance().reference
+
+        // FirebaseAuthのオブジェクトを取得する
+        mAuth = FirebaseAuth.getInstance()
+
+        mCreateAccountListener = OnCompleteListener { task ->
+            //
+        }
+
+        mLoginListener = OnCompleteListener { task ->
+            //
+        }
+
+        title = ""
+
+        createButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        loginButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
     }
+
+    private fun createAccount(email:String, password:String){
+
+    }
+
+    private fun login(email:String, password: String){
+
+    }
+
+    private fun saveName(name:String){
+        val sp = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = sp.edit()
+        editor.putString(NameKEY, name)
+        editor.commit()
+    }
+
 }
