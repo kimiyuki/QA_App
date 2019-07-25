f72d0e36f656b5f90d607cfeff04159f5af53db6
kimiyuki shirai <kimiyuki@gmail.com>
2019-07-25 13:50:27 +0900

    refactor created functions

--- app/src/main/java/shirai/kimiyuki/techacademy/qa_app/QuestionSendActivity.kt
+++ app/src/main/java/shirai/kimiyuki/techacademy/qa_app/QuestionSendActivity.kt
@@ -1,6 +1,5 @@
 package shirai.kimiyuki.techacademy.qa_app
 
-import android.app.Instrumentation
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -19,10 +18,10 @@ import android.preference.PreferenceManager
 import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.util.Base64
-import android.util.Log
 import android.view.View
-import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -90,58 +89,64 @@ class QuestionSendActivity : AppCompatActivity(), View.OnClickListener, Database
             imageView.setImageBitmap(resizeImage)
             mPictureUri = null
         }
-
     }
 
     override fun onClick(v: View?) {
-        if(v === imageView){
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                    showChooser()
-                }else{
-                    requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
-                    return
-                }
-            }else{ showChooser() }
-        }else if(v === sendButton) {
-            val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+        when(v){
+            imageView -> imageViewOnClick(v as ImageView)
+            sendButton -> sendButtonOnClick(v as Button)
+        }
+    }
 
-            val databaseReference = FirebaseDatabase.getInstance().reference
-            val genreRef = databaseReference.child(ContentsPATH).child(mGenre.toString())
-
-            val data = HashMap<String, String>()
-
-            data["uid"] = FirebaseAuth.getInstance().currentUser!!.uid
-
-            val title = titleText.text.toString()
-            val body = bodyText.text.toString()
-
-            if(title.isEmpty()){
-                Snackbar.make(v, "タイトルを入力してください", Snackbar.LENGTH_LONG).show()
+    private fun imageViewOnClick(v: ImageView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                showChooser()
+            } else {
+                requestPermissions( arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE )
                 return
             }
+        } else { showChooser() }
+    }
 
-            val sp = PreferenceManager.getDefaultSharedPreferences(this)
-            val name = sp.getString(NameKEY, "")
+    private fun sendButtonOnClick(v: Button) {
+        val im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
 
-            data["title"] = title
-            data["body"] = body
-            data["name"] = name
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        val genreRef = databaseReference.child(ContentsPATH).child(mGenre.toString())
 
-            val drawable = imageView.drawable as? BitmapDrawable
+        val data = HashMap<String, String>()
 
-            if(drawable != null){
-                val bitmap = drawable.bitmap
-                val baos = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos)
-                val bitmapString = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT)
+        data["uid"] = FirebaseAuth.getInstance().currentUser!!.uid
 
-                data["image"] = bitmapString
-            }
-            genreRef.push().setValue(data, this)
-            progressBar.visibility = View.VISIBLE
+        val title = titleText.text.toString()
+        val body = bodyText.text.toString()
+
+        if (title.isEmpty()) {
+            Snackbar.make(v, "タイトルを入力してください", Snackbar.LENGTH_LONG).show()
+            return
         }
+
+        val sp = PreferenceManager.getDefaultSharedPreferences(this)
+        val name = sp.getString(NameKEY, "")
+
+        data["title"] = title
+        data["body"] = body
+        data["name"] = name
+
+        val drawable = imageView.drawable as? BitmapDrawable
+
+        if (drawable != null) {
+            val bitmap = drawable.bitmap
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos)
+            val bitmapString = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT)
+
+            data["image"] = bitmapString
+        }
+        genreRef.push().setValue(data, this)
+        progressBar.visibility = View.VISIBLE
     }
 
     override fun onComplete(databaseError: DatabaseError?, databaseRef: DatabaseReference) {
